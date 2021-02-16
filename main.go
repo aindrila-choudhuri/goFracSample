08b122bfc7326c37c565a769fa3992c07a41a5a9
aindrila-choudhuri <choudhuri.aindrila@gmail.com>
2021-02-16 14:24:59 +0530

    5/n refactored code and test cases

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 	"strings"
 )
 
+const (
+	infiniteString  = "infinite"
+	undefinedString = "undefined"
+)
+
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
@@ -20,16 +25,19 @@ func main() {
 	var fracText []string
 
 	text, _ := reader.ReadString('\n')
-	text = strings.Replace(text, "\n", "", -1)
+	text = strings.Trim(strings.Replace(text, "\n", "", -1), " ")
 
-	numLen, err := strconv.Atoi(text)
+	numLen, err := strconv.Atoi(strings.Trim(text, " "))
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		for i := 0; i < numLen; i++ {
 			fmt.Printf("Enter number %d in fraction format: ", i+1)
 			num, _ := reader.ReadString('\n')
-			num = strings.Replace(num, "\n", "", -1)
+			num = strings.Trim(strings.Replace(num, "\n", "", -1), " ")
+			if !isValidInput(num) {
+				return
+			}
 			if len(num) > 0 {
 				fracText = append(fracText, num)
 			}
@@ -39,62 +47,76 @@ func main() {
 	fmt.Println("Result is : ", res)
 }
 
+// isValidInput checks whether / is in correct place
+func isValidInput(numText string) bool {
+	if strings.Index(numText, "/") < 1 || strings.Count(numText, "/") > 1 {
+		fmt.Println("Oops! You need to enter the numbers in fraction format")
+		return false
+	}
+
+	return true
+}
+
 // multiplyFractions takes fractions in string format and multiply them.
 func multiplyFractions(fractions ...string) string {
-	var (
-		numArr []int
-		denArr []int
-	)
-
 	numMul := 1
 	denMul := 1
 
 	if len(fractions) > 0 {
 		for i := 0; i < len(fractions); i++ {
-			getFractionArrays(fractions[i], &numArr, &denArr)
+			getFractionMultiplicationResult(fractions[i], &numMul, &denMul)
 		}
 	}
 
-	for i := 0; i < len(numArr); i++ {
-		numMul = numMul * numArr[i]
-	}
-
-	for i := 0; i < len(denArr); i++ {
-		denMul = denMul * denArr[i]
-	}
+	// wg := &sync.WaitGroup{}
+	// if len(fractions) > 0 {
+	// 	wg.Add(len(fractions))
+	// 	for i := 0; i < len(fractions); i++ {
+	// 		go func(fraction string, numMul *int, denMul *int) {
+	// 			getFractionMultiplicationResult(fraction, numMul, denMul)
+	// 			wg.Done()
+	// 		}(fractions[i], &numMul, &denMul)
+	// 	}
+	// 	wg.Wait()
+	// }
 
 	numMul, denMul = reduce(numMul, denMul)
 
-	n1 := strconv.Itoa(numMul)
-	d1 := strconv.Itoa(denMul)
+	res := ""
 
-	return n1 + "/" + d1
+	if numMul == 0 {
+		res = infiniteString
+	} else if denMul == 0 {
+		res = undefinedString
+	} else {
+		n1 := strconv.Itoa(numMul)
+		d1 := strconv.Itoa(denMul)
+		res = n1 + "/" + d1
+	}
+
+	return res
 }
 
-// getFractionArrays receives the fraction in string format, splits into numerator and denominator, pushes to appropriate arrays and returns
-func getFractionArrays(fraction string, numArr *[]int, denArr *[]int) {
-	if strings.Index(fraction, "/") > -1 {
-		fracArr := strings.Split(fraction, "/")
-		num, numErr := strconv.Atoi(fracArr[0])
-		if numErr != nil {
-			log.Fatal(numErr)
-		} else {
-			*numArr = append(*numArr, num)
-		}
-
-		den, denErr := strconv.Atoi(fracArr[1])
-		if denErr != nil {
-			log.Fatal(denErr)
-		} else {
-			*denArr = append(*denArr, den)
-		}
+// getFractionMultiplicationResult splits each fraction into numerator and denominator and multiplies
+func getFractionMultiplicationResult(fraction string, numMul, denMul *int) {
+	fracArr := strings.Split(fraction, "/")
+	num, numErr := strconv.Atoi(fracArr[0])
+	if numErr != nil {
+		log.Fatal(numErr)
 	} else {
-		log.Fatal("Oops! You need to enter the numbers in fraction format")
+		*numMul = *numMul * num
+	}
+
+	den, denErr := strconv.Atoi(fracArr[1])
+	if denErr != nil {
+		log.Fatal(denErr)
+	} else {
+		*denMul = *denMul * den
 	}
 }
 
 // gcd will determine the greatest common divisor
-func gcd(a int, b int) int {
+func gcd(a, b int) int {
 	if a == 0 {
 		return b
 	}
@@ -110,7 +132,7 @@ func gcd(a int, b int) int {
 	return gcd(a, b-a)
 }
 
-// reduce will determine the greatest common factor (GCF)
+// reduce reduces the fraction to its lowest form
 func reduce(nums ...int) (int, int) {
 	ans := nums[0]
 	for i := 1; i < len(nums); i++ {
--- main_test.go
+++ main_test.go
@@ -10,8 +10,13 @@ func TestTableMultiplyFractions(t *testing.T) {
 		{[]string{"3/4", "5/6"}, "5/8"},
 		{[]string{"56/78", "23/67"}, "644/2613"},
 		{[]string{"2/3", "5/8", "11/32"}, "55/384"},
+		{[]string{"2/3", "3/4", "4/5", "5/6", "6/7", "7/8", "8/9", "9/10", "10/11", "2/5"}, "4/55"},
+		{[]string{"3/5", "7/8", "2/6", "7/66", "12/13", "4/12", "39/60", "23/46", "6/13", "12/67"}, "147/958100"},
+		{[]string{"3/5", "7/8", "2/6", "7/66", "12/13", "4/12", "39/60", "23/46", "6/13", "12/67", "2/9", "11/13"}, "49/1698450"},
+		{[]string{"3/4", "5/0"}, undefinedString},
+		{[]string{"3/4", "0/6"}, infiniteString},
 	}
-
+	//{[]string{"2/3", "3/4", "4/5", "5/6", "6/7", "7/8", "8/9", "9/10", "10/11", "2/5"}, "4/55"},
 	for _, test := range tests {
 		if output := multiplyFractions(test.inputFracs...); output != test.expected {
 			t.Error("Test failed: {} inputted, {} expected, received: {}", test.inputFracs, test.expected, output)
